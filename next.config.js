d9d848f039f5067279223e7fb674f6a7e2d3ea15
vipinvkartha <vipin0292@gmail.com>
2024-12-30 23:27:34 +0100

    fix

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  eslint: {
-    ignoreDuringBuilds: true,
+  basePath: '/sattva-wellness',
+  images: {
+    unoptimized: true,
   },
-  images: { unoptimized: true },
-};
+  assetPrefix: '/sattva-wellness/',
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
