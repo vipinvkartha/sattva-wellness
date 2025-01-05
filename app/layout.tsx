cf061064906a9f1d9bf4074583dd133995280515
vipinvkartha <vipin0292@gmail.com>
2025-01-05 20:07:58 +0100

    add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -67,7 +67,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="canonical" href="https://sattvawellness.vercel.app" />
+        <link rel="canonical" href="https://sattva-wellness.in" />
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
         <link rel="icon" href="/favicon.ico" />
         <Script
